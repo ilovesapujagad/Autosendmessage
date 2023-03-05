4cc9f60d331255b20734cf33cf787662ead97f1b
lovesapujagad <118962746+ilovesapujagad@users.noreply.github.com>
2023-03-05 23:50:57 +0700

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ if api_key == "":
     print("api key is required", file=stderr)
 
 api_base_url = "https://api.stagingv3.microgen.id/query/api/v1/" + api_key
-
+print("s")
 @app.route('/')
 def hello_geek():
     return '<h1>Hello from Flask</h2>'
