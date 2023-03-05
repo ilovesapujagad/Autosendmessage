e960c2a731d2decf0a96095fa7b740070d203583
lovesapujagad <118962746+ilovesapujagad@users.noreply.github.com>
2023-03-05 23:52:09 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,145 +1,61 @@
-import json
-import os
+import paramiko
+import time
+import sys
+import socket
 import requests
-from sys import stderr
-from flask import Flask, request, jsonify
+import json
+# app = Flask(__name__)
 
-app = Flask(__name__)
 
-api_key = os.environ.get("API_KEY", "")
-if api_key == "":
-    print("api key is required", file=stderr)
 
-api_base_url = "https://api.stagingv3.microgen.id/query/api/v1/" + api_key
-print("s")
-@app.route('/')
-def hello_geek():
-    return '<h1>Hello from Flask</h2>'
+# @app.get('/message')
+# def messegae():
+while True:
+    url = "https://database-query.v3.microgen.id/api/v1/fb6db565-2e6c-41eb-bf0f-66f43b2b75ae/KafkaConnect?$select[0]=connector&$select[1]=createdBy"
+    lists = requests.get(url)
+    n = len(lists.json())
+    for i in range(0, n):
+        def ssh_con (ip, un, pw):
+            global client
+            client = paramiko.SSHClient()
+            client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            # print ("Connecting to device/VM: %s" % ip)
+            client.connect(ip, username=un, password=pw,timeout=10)
 
-@app.get("/products")
-def getProducts():
-    try:
-        url = "/".join([api_base_url, "products"])
-        response = requests.get(url)
-        respBody = response.json()
 
-        if response.status_code != 200:
-            if respBody.get('message') == 'project not found':
-                respJson = jsonify(
-                    {"message": "failed to connect to your project, please check if the api had been set properly."}, 
-                )
-                respJson.status_code = response.status_code
+        def cmd_io (command):
+            global client_cmd
+            client_cmd.send("%s \n" %command)
+            time.sleep(1)
+            output = client_cmd.recv(10000).decode("utf-8")
+            print (output)
 
-                return respJson
+        ip = '10.10.65.5'
+        un = 'sapujagad'
+        pw = 'kayangan'
 
-            respJson = jsonify(respBody)
-            respJson.status_code = response.status_code
-
-            return respJson
-
-        return jsonify(respBody)
-    except Exception as e:
-        return jsonify({"message": "error occured: " + e.__str__()})
-
-@app.post("/products")
-def createProduct():
-    try:
-        url = "/".join([api_base_url, "products"])
-        response = requests.post(url, json.dumps(request.json, indent=2))
-        respBody = response.json()
-
-        if response.status_code != 201:
-            if respBody.get('message') == 'project not found':
-                respJson = jsonify(
-                    {"message": "failed to connect to your project, please check if the api had been set properly."}, 
-                )
-                respJson.status_code = response.status_code
-
-                return respJson
-
-            respJson = jsonify(respBody)
-            respJson.status_code = response.status_code
-
-            return respJson
-        
-        return jsonify(respBody)
-    except Exception as e:
-        return jsonify({"message": "error occured: " + e.__str__()})
-
-@app.get("/products/<id>")
-def getProductById(id):
-    try:
-        url = "/".join([api_base_url, "products", id])
-        response = requests.get(url)
-        respBody = response.json()
-
-        if response.status_code != 200:
-            if respBody.get('message') == 'project not found':
-                respJson = jsonify(
-                    {"message": "failed to connect to your project, please check if the api had been set properly."}, 
-                )
-                respJson.status_code = response.status_code
-
-                return respJson
-
-            respJson = jsonify(respBody)
-            respJson.status_code = response.status_code
-
-            return respJson
-
-        return jsonify(respBody)
-    except Exception as e:
-        return jsonify({"message": "error occured: " + e.__str__()})
-
-@app.patch("/products/<id>")
-def updateProduct(id):
-    try:
-        url = "/".join([api_base_url, "products", id])
-        response = requests.patch(url, json.dumps(request.json, indent=2))
-        respBody = response.json()
-
-        if response.status_code != 200:
-            if respBody.get('message') == 'project not found':
-                respJson = jsonify(
-                    {"message": "failed to connect to your project, please check if the api had been set properly."}, 
-                )
-                respJson.status_code = response.status_code
-
-                return respJson
-
-            respJson = jsonify(respBody)
-            respJson.status_code = response.status_code
-
-            return respJson
-        
-        return jsonify(respBody)
-    except Exception as e:
-        return jsonify({"message": "error occured: " + e.__str__()})
-
-@app.delete("/products/<id>")
-def deleteProduct(id):
-    try:
-        url = "/".join([api_base_url, "products", id])
-        response = requests.delete(url)
-        respBody = response.json()
-
-        if response.status_code != 200:
-            if respBody.get('message') == 'project not found':
-                respJson = jsonify(
-                    {"message": "failed to connect to your project, please check if the api had been set properly."}, 
-                )
-                respJson.status_code = response.status_code
-
-                return respJson
-
-            respJson = jsonify(respBody)
-            respJson.status_code = response.status_code
-
-            return respJson
-
-        return jsonify(respBody)
-    except Exception as e:
-        return jsonify({"message": "error occured: " + e.__str__()})
-
-if __name__ == "__main__":
-    app.run(debug=True)
+        ssh_con(ip,un,pw)
+        client_cmd = client.invoke_shell()
+        command = "sudo docker exec -it ubuntu-kafka /opt/confluent/bin/kafka-avro-console-consumer --topic "+lists.json()[i]['connector']+"-XE-redo-log"+" --bootstrap-server 10.10.65.5:9092 --from-beginning" 
+        cmd_io (command)
+        client_cmd.settimeout(60.0)
+        progress = True
+        messegae = 0
+        urlen = "https://database-query.v3.microgen.id/api/v1/fb6db565-2e6c-41eb-bf0f-66f43b2b75ae/Kafka_Messages?$select[0]=topic&topic="+lists.json()[i]['connector']+""
+        response = requests.get(urlen)
+        lenmessage = len(response.json)
+        while progress:
+            try:
+                output = client_cmd.recv(100000).decode("utf-8")
+                messegae += 1
+                print (messegae)
+                if lenmessage == messegae:
+                    url = "https://database-query.v3.microgen.id/api/v1/fb6db565-2e6c-41eb-bf0f-66f43b2b75ae/Kafka_Messages"
+                    jsons = {"topic":lists.json()[i]['connector'],"messages":output,"createdBy":lists.json()[i]['createdBy']['firstName']+" "+lists.json()[i]['createdBy']['lastName']}
+                    response = requests.post(url,json=jsons)
+                    print (response.status_code)
+            except socket.timeout:
+                progress = False
+                messegae = 0
+                pass
+    client.close()
